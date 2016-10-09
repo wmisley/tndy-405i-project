3d937d68d7de91a03f15c59d7e7a9143a9e29f00
will4769 <wisley@esri.com>
2016-10-09 11:46:21 -0700

    Removed Twitter keys

--- twitter_query.py
+++ twitter_query.py
@@ -2,10 +2,10 @@
 
 import tweepy
 
-consumer_key = "n2WebZuPpCWG9xguQ9Nx6sJLW"
-consumer_secret = "WTfshJzVzrSaWuT4hjEtlV6KivAQa79unN9eCCGIrtduQtQ8vr"
-access_token = "70616645-l81XYFgtS7E9a2WsrlC5OHE8OqM2ywC3aV28MPMMN"
-access_token_secret = "67r1dgZvJwhse1z8hl026pSsZESwkuzVZfRekBdvvqTct"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
